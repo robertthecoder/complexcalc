511bf78eeec2e1d41d340f276ca34d4939d5aedd
Robert J. Kim <robert7kim7@gmail.com>
2018-01-22 23:58:44 -0800

    first version

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,40 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs + rhs;
+        
+        return temp;
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs - rhs;
+        
+        return temp;
+    }
+
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs * rhs;
+        return temp;
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs / rhs;
+        return temp;
+    }
+    
+    func count(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs / rhs;
+        return temp;
+    }
+    
+    func mathOp(lhs: Int, rhs: Int) -> Int {
+        var temp = lhs / rhs;
+        return temp;
+    }
+    
+    func avg(array: []) -> Int {
+        return 3;
+    }
+    
 }
